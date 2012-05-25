9b1f393dcee4cddebc9a37f1aed2fea4d717e949
Anthony Crumley <anthony.crumley@gmail.com>
2012-05-25 11:27:26 -0500

    Automatically generated rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fellowship::Application.config.secret_token = 'f201a7167ff416bed8730ff50f03993ef674c3233343dfd4d22377973f69fa7feed7b0295d261f632d9be4d42fbb5773e9686f8af883349f709dc9c9b4dcb7e9'
